f94054310c8d79c0d0ecb8f5bf49925b1d624ce6
MohammadhRimaz <mohammadhrimaz@gmail.com>
2025-03-12 17:35:54 +0530

    Add GitHub commit integration and update Prisma schema for commit tracking (1.42.09): Clear

---
+++ dionysus/src/lib/github.ts
@@ -0,0 +1,80 @@
+// bun run src/lib/github.ts
+// npx tsx src/lib/github.ts
+
+import { db } from "@/server/db";
+import { Octokit } from "octokit";
+
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const githubUrl = "https://github.com/docker/genai-stack";
+
+type Response = {
+  commitHash: string;
+  commitMessage: string;
+  commitAuthorName: string;
+  commitAuthorAvatar: string;
+  commitDate: string;
+};
+
+export const getCommitHashes = async (
+  githubUrl: string,
+): Promise<Response[]> => {
+  const { data } = await octokit.rest.repos.listCommits({
+    owner: "docker",
+    repo: "genai-stack",
+  });
+
+  const sortedCommits = data.sort(
+    (a: any, b: any) =>
+      new Date(b.commit.author.date).getTime() -
+      new Date(a.commit.author.date).getTime(),
+  ) as any[];
+  return sortedCommits.slice(0, 10).map((commit: any) => ({
+    commitHash: commit.sha as string,
+    commitMessage: commit.commit.message ?? "",
+    commitAuthorName: commit.commit?.author?.name ?? "",
+    commitAuthorAvatar: commit.author?.avatar_url ?? "",
+    commitDate: commit.commit?.author?.date ?? "",
+  }));
+};
+
+export const pollCommits = async (projectId: string) => {
+  const { project, githubUrl } = await fetchProjectGithubUrl(projectId);
+  const commitHashes = await getCommitHashes(githubUrl);
+  const unprocessedCommits = await filterUnprocessedCommits(
+    projectId,
+    commitHashes,
+  );
+  return unprocessedCommits;
+};
+
+async function fetchProjectGithubUrl(projectId: string) {
+  const project = await db.project.findUnique({
+    where: { id: projectId },
+    select: { githubUrl: true },
+  });
+  if (!project?.githubUrl) {
+    throw new Error("Project has no GitHub URL");
+  }
+  return { project, githubUrl: project?.githubUrl };
+}
+
+async function filterUnprocessedCommits(
+  projectId: string,
+  commitHashes: Response[],
+) {
+  const processedCommits = await db.commit.findMany({
+    where: { projectId },
+  });
+  const unprocessedCommits = commitHashes.filter(
+    (commit) =>
+      !processedCommits.some(
+        (processedCommits) => processedCommits.commitHash === commit.commitHash,
+      ),
+  );
+  return unprocessedCommits;
+}
+
+await pollCommits("cm85muvtx0000kvx0d76tsm9n").then(console.log);
