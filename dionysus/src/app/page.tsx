9cfd78653c1ccf195086ef1b452297ae6c15659c
MohammadhRimaz <mohammadhrimaz@gmail.com>
2025-04-12 19:08:24 +0530

    Refactor sidebar items and update layout icon; enhance home page structure

--- dionysus/src/app/page.tsx
+++ dionysus/src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button>Click me</Button>
+    <div className="flex min-h-screen flex-col items-center justify-center p-4">
+      <h1>Hello Dionysus!</h1>
+      <Link href="/dashboard">
+        <Button>Click me</Button>
+      </Link>
     </div>
   );
 }
